509ee51ee96b5271bcc4655677257fcd804b81d3
JuHyun Seong <estel.castle@gmail.com>
2016-03-16 20:38:35 +0900

    Spring In Action4 4장 AOP 실습(WIP)
    ...

---
+++ spring/sia4/aopdemo/src/test/java/com/chap4/aop/AopdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chap4.aop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AopdemoApplication.class)
+public class AopdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
