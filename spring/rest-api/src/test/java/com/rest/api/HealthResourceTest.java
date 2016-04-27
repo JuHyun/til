a4252b83af5633ebb20197078ad963753b0ce9a2
JuHyun Seong <estel.castle@gmail.com>
2016-04-27 21:58:13 +0900

    JSON 형태로 결과가 나오도록 API 수정
    - text/plain에서 json 형태의 결과가 나오도록 수정.
    - Health Entity 추가함.
    - HelloWorldResource를 HealthResource로 변경.

--- spring/rest-api/src/test/java/com/rest/api/HealthResourceTest.java
+++ spring/rest-api/src/test/java/com/rest/api/HealthResourceTest.java
@@ -2,33 +2,33 @@ package com.rest.api;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.rest.domain.Health;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * HelloWorld 리소스 테스트.
+ * Health Resource Test.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-public class HelloWorldResourceTest {
+public class HealthResourceTest {
 
-    private static final String HOST_NAME = "http://localhost:8080/api";
-
-    private static final String HELLO_WORLD = "Hello World";
+    private static final String ACCESS_URL = "http://localhost:8080/api/health";
 
     private RestTemplate restTemplate = new TestRestTemplate();
 
     @Test
-    public void hello() {
-        String accessUrl = HOST_NAME + "/hello/world";
+    public void health() {
+        ResponseEntity<Health> responseEntity =
+                restTemplate.getForEntity(ACCESS_URL, Health.class);
 
-        String result = restTemplate.getForObject(accessUrl, String.class);
-
-        assertThat(result).isEqualTo(HELLO_WORLD);
+        assertThat(responseEntity.getStatusCode().is2xxSuccessful()).isTrue();
+        assertThat(responseEntity.getBody().getStatus()).isEqualTo("Strong");
     }
 }
