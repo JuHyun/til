ab3946c5310a3e7b6b4ac7f510a6b856177f768b
Estel Seong <estel.castle@gmail.com>
2016-05-02 23:40:28 +0900

    SpringMVC RESTful API 프로젝트 initail
    SpringMVC RESTful API 프로젝트 최초 생성.

---
+++ spring/springmvc-rest-api/src/test/java/com/springmvc/SpringmvcRestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringmvcRestApiApplication.class)
+public class SpringmvcRestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
