e65c2fb9533c1a85ddfccf86f887d76eb0afe6ad
Estel Seong <estel.castle@gmail.com>
2018-06-26 00:01:49 +0900

    default content negotiation 설정 테스트. - application/json으로 모두 produce하게 하고 싶을 때, 공통으로 적용하려고.

---
+++ spring/content-negotiation/src/main/java/com/example/contentnegotiation/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.contentnegotiation.controller;
+
+import com.example.contentnegotiation.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public @ResponseBody
+    User user() {
+        return new User("성주현", 35);
+    }
+}
