7404646fc38b5e0dc733cd106dd0c61b69950c16
Estel Seong <estel.castle@gmail.com>
2019-01-08 00:06:21 +0900

    [Algorithm] 앞뒤로 뒤집었을 때 동일한 문자열이 되도록 만들기(Palindrome).

--- interview-practice/src/Palindrome.java
+++ interview-practice/src/Palindrome.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Palindrome {
 
     public static boolean isPalindrome(String word) {
@@ -15,5 +18,35 @@ public class Palindrome {
             }
         }
         return true;
+    }
+
+    public static String makePalindrome(String string, int l, int h) {
+        if (l >= h) {
+            return string;
+        }
+        if (string.charAt(l) == string.charAt(h)) {
+            return makePalindrome(string, l + 1, h - 1);
+        } else {
+            StringBuilder stringBuilder = new StringBuilder();
+            if (h == string.length() - 1) {
+                stringBuilder.append(string);
+                stringBuilder.append(string.charAt(l));
+                string = stringBuilder.toString();
+            } else {
+                stringBuilder.append(string, 0, h + 1);
+                stringBuilder.append(string.charAt(l));
+                stringBuilder.append(string, h + 1, string.length());
+                string = stringBuilder.toString();
+            }
+            return makePalindrome(string, l + 1, h);
+        }
+    }
+
+    private static List<Character> converStringToList(String string) {
+        List<Character> linkedList = new LinkedList<>();
+        for (int i = 0; i < string.length(); i++) {
+            linkedList.add(string.charAt(i));
+        }
+        return linkedList;
     }
 }
--- interview-practice/test/PalindromeTest.java
+++ interview-practice/test/PalindromeTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -9,5 +10,20 @@ public class PalindromeTest {
     public void isPalindrome() {
         assertTrue(Palindrome.isPalindrome("Deleveled"));
         assertFalse(Palindrome.isPalindrome("abcde"));
+    }
+
+    @Test
+    public void shouldMakePalindrome() {
+        assertEquals("abcba", Palindrome.makePalindrome("abc", 0, 2));
+        assertEquals("thereht", Palindrome.makePalindrome("there", 0, 4));
+        assertEquals("abcdcba", Palindrome.makePalindrome("abcda", 0, 4));
+    }
+
+    @Test
+    public void test() {
+        StringBuilder stringBuilder = new StringBuilder();
+        String str = "abcd";
+        stringBuilder.append(str, 0, 3);
+        assertEquals("abc", stringBuilder.toString());
     }
 }
