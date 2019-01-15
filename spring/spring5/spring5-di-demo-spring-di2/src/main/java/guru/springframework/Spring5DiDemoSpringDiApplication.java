e3120d21da1050642236013c53f76e2c3a902a8a
Estel Seong <estel.castle@gmail.com>
2019-01-15 22:26:24 +0900

    spring framework context.

--- spring/spring5/spring5-di-demo-spring-di2/src/main/java/guru/springframework/Spring5DiDemoSpringDiApplication.java
+++ spring/spring5/spring5-di-demo-spring-di2/src/main/java/guru/springframework/Spring5DiDemoSpringDiApplication.java
@@ -1,5 +1,7 @@
 package guru.springframework;
 
+import guru.springframework.controllers.MyContoller;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,7 +12,9 @@ public class Spring5DiDemoSpringDiApplication {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 SpringApplication.run(Spring5DiDemoSpringDiApplication.class, args);
-    }
 
+        MyContoller myContoller = (MyContoller) applicationContext.getBean("myContoller");
+        myContoller.hello();
+    }
 }
 
