705201a02b7707a04df3ce27ac26463debf12fb2
Estel Seong <estel.castle@gmail.com>
2019-02-06 12:48:49 +0900

    initial commit 2.

---
+++ spring/spring5/sfg-pet-clinic/pet-clinic-web/src/test/java/practice/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.springframework.sfgpetclinic;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SfgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
