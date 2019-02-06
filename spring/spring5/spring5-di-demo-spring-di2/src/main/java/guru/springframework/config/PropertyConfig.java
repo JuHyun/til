9e90303eca109f0af30b25a024343143f7230b05
Estel Seong <estel.castle@gmail.com>
2019-02-06 23:28:06 +0900

    Loading Spring Boot Application.properties
    - By default, Spring Boot loads application.properties.
    - So only needs @Configuration, @Value

--- spring/spring5/spring5-di-demo-spring-di2/src/main/java/guru/springframework/config/PropertyConfig.java
+++ spring/spring5/spring5-di-demo-spring-di2/src/main/java/guru/springframework/config/PropertyConfig.java
@@ -3,27 +3,12 @@ package guru.springframework.config;
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJMSBroker;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-// 방법1.
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-// 방법2.
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    @Autowired
-    private Environment environment;
 
     @Value("${guru.username}")
     private String username;
@@ -46,7 +31,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(environment.getProperty("GURU_TENNIS_PLAYER"));
+        fakeDataSource.setUsername(username);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
@@ -59,12 +44,5 @@ public class PropertyConfig {
         jmsBroker.setPassword(jmsPassword);
         jmsBroker.setUsername(jmsUrl);
         return jmsBroker;
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =
-                new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
     }
 }
