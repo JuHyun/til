6bc07754d4ed26f06de97f8eabb10535ff8c7ad1
Estel Seong <estel.castle@gmail.com>
2019-02-06 21:41:14 +0900

    Listing All Owners in View.

--- spring/spring5/sfg-pet-clinic/pet-clinic-web/src/main/java/practice/springframework/sfgpetclinic/controllers/OwnerController.java
+++ spring/spring5/sfg-pet-clinic/pet-clinic-web/src/main/java/practice/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,15 +1,28 @@
 package practice.springframework.sfgpetclinic.controllers;
 
+import practice.springframework.sfgpetclinic.model.Owner;
+import practice.springframework.sfgpetclinic.services.OwnerSerivce;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private OwnerSerivce ownerSerivce;
+
+    public OwnerController(OwnerSerivce ownerSerivce) {
+        this.ownerSerivce = ownerSerivce;
+    }
+
     @GetMapping({"", "/index", "/index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("owners", ownerSerivce.findAll());
         return "owners/index";
     }
 }
