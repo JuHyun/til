bb09532f0ea266b06ad75b56dba14ba18621a057
Estel Seong <estel.castle@gmail.com>
2019-02-09 18:41:33 +0900

    Add JPA Query Method.

--- spring/spring5/recipe/src/main/java/practice/springframework/mvc/recipe/controllers/IndexController.java
+++ spring/spring5/recipe/src/main/java/practice/springframework/mvc/recipe/controllers/IndexController.java
@@ -1,15 +1,37 @@
 package practice.springframework.mvc.recipe.controllers;
 
+import practice.springframework.mvc.recipe.domain.Category;
+import practice.springframework.mvc.recipe.domain.UnitOfMeasure;
+import practice.springframework.mvc.recipe.repositories.CategoryRepository;
+import practice.springframework.mvc.recipe.repositories.UnitOfMeasureRepository;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @GetMapping({"", "/index", "/index"})
     public String index() {
+        Optional<Category> italian = categoryRepository.findByDescription("Italian");
+        Optional<UnitOfMeasure> ounce = unitOfMeasureRepository.findByDescription("Ounce");
+
+        System.out.println(italian.get().getId());
+        System.out.println(ounce.get().getId());
+
         return "index";
     }
 }
--- spring/spring5/recipe/src/main/java/practice/springframework/mvc/recipe/repositories/CategoryRepository.java
+++ spring/spring5/recipe/src/main/java/practice/springframework/mvc/recipe/repositories/CategoryRepository.java
@@ -4,5 +4,9 @@ import practice.springframework.mvc.recipe.domain.Category;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
