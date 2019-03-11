4a1c4f4992a60da82a96440038af0b1e659f9642
Estel Seong <estel.castle@gmail.com>
2019-03-11 20:50:21 +0900

    String Literal vs String Object.
    String Constant Pool.
    - Java7에서 String Constant Pool이 PermGen 영역에서 Heap 영역으로 옮겨갔다고 함.
    - 이유는 PermGem은 사이즈가 고정되어 있어서 Runtime 시에 늘릴 수 없기 때문에 OutOfMemoryException이 발생하는 일이 잦나봄.
    - 그래서 Heap 영역으로 옮겨갔다고 함. 이 말은 즉, GC에 의해서 제거될 수 있게 된 거임.
    - 참조: https://medium.com/@joongwon/string-%EC%9D%98-%EB%A9%94%EB%AA%A8%EB%A6%AC%EC%97%90-%EB%8C%80%ED%95%9C-%EA%B3%A0%EC%B0%B0-57af94cbb6bc

--- interview-practice/src/test/java/design/pattern/flyweight/FlyweightTest.java
+++ interview-practice/src/test/java/design/pattern/flyweight/FlyweightTest.java
@@ -1,7 +1,9 @@
 package design.pattern.flyweight;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +17,21 @@ public class FlyweightTest {
 
         assertNotSame(Integer.valueOf(128), Integer.valueOf(128));
         assertNotSame(Integer.valueOf(-200), Integer.valueOf(-200));
+    }
+
+    @Test
+    public void checkStringInternPool() {
+        final String string1 = new String("abcd");
+        final String string2 = new String("abcd");
+        final String stringLiteral1 = "abcd";
+        final String stringLiteral2 = "abcd";
+
+        assertTrue(string1.equals(string2));
+        assertTrue(string1.equals(stringLiteral1));
+        assertFalse(string1 == string2);
+
+        assertFalse(string1 == stringLiteral1);
+        assertTrue(stringLiteral1.equals(stringLiteral2));
+        assertTrue(stringLiteral1 == stringLiteral2);
     }
 }
