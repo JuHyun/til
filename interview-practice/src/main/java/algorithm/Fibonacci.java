4eaf028295ecbc0c40879757b459afc5f8075367
Estel Seong <estel.castle@gmail.com>
2019-03-12 08:36:40 +0900

    [Algorithm] Fibonacci using Dynamic Programming.

--- interview-practice/src/main/java/algorithm/Fibonacci.java
+++ interview-practice/src/main/java/algorithm/Fibonacci.java
@@ -8,4 +8,15 @@ public class Fibonacci {
         }
         return fibonacci(n - 2) + fibonacci(n - 1);
     }
+
+    public static int dynamicFibonacci(int n) {
+        final int result[] = new int[n + 2];
+        result[0] = 0;
+        result[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        return result[n];
+    }
 }
--- interview-practice/src/test/java/algorithm/FibonacciTest.java
+++ interview-practice/src/test/java/algorithm/FibonacciTest.java
@@ -15,4 +15,14 @@ public class FibonacciTest {
         assertEquals(5, Fibonacci.fibonacci(5));
         assertEquals(8, Fibonacci.fibonacci(6));
     }
+
+    @Test
+    public void dynamicFibonacchi() {
+        assertEquals(1, Fibonacci.dynamicFibonacci(1));
+        assertEquals(1, Fibonacci.dynamicFibonacci(2));
+        assertEquals(2, Fibonacci.dynamicFibonacci(3));
+        assertEquals(3, Fibonacci.dynamicFibonacci(4));
+        assertEquals(5, Fibonacci.dynamicFibonacci(5));
+        assertEquals(8, Fibonacci.dynamicFibonacci(6));
+    }
 }
