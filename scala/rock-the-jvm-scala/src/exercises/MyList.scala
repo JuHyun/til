7f1857ca1364763830e58c1675e1fcf9efa3a018
성주현 <espel@onestorecorp.com>
2019-06-18 19:19:21 +0900

    map, filter, flapMap

--- scala/rock-the-jvm-scala/src/exercises/MyList.scala
+++ scala/rock-the-jvm-scala/src/exercises/MyList.scala
@@ -13,6 +13,15 @@ abstract class MyList[+A] {
   def printElements: String
 
   override def toString: String = "[" + printElements + "]"
+
+  def map[B](transformer: MyTransform[A, B]): MyList[B]
+
+  def flatMap[B](transformer: MyTransform[A, MyList[B]]): MyList[B]
+
+  def filter(predicate: MyPredicate[A]): MyList[A]
+
+  def ++[B >: A](list: MyList[B]): MyList[B]
+
 }
 
 object Empty extends MyList[Nothing] {
@@ -25,6 +34,14 @@ object Empty extends MyList[Nothing] {
   override def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
 
   override def printElements: String = ""
+
+  override def map[B](transformer: MyTransform[Nothing, B]): MyList[B] = Empty
+
+  override def flatMap[B](transformer: MyTransform[Nothing, MyList[B]]): MyList[B] = Empty
+
+  override def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+
+  override def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
 class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -40,6 +57,40 @@ class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     if (tail.isEmpty) head + " "
     else head + " " + tail.printElements
   }
+
+  override def map[B](transformer: MyTransform[A, B]): MyList[B] =
+    new Cons(transformer.transform(h), t.map(transformer))
+
+  /**
+    * [1, 2].flatMap(n => [n, n+1])
+    * = [1, 2] ++ 2.flatMap(n => [n, n+1])
+    * = [1, 2] ++ [2, 3] ++ Empty.flatMap(n => [n, n+1])
+    * = [1,2,2,3]
+    **/
+  override def flatMap[B](transformer: MyTransform[A, MyList[B]]): MyList[B] = {
+    transformer.transform(h) ++ t.flatMap(transformer)
+  }
+
+  override def filter(predicate: MyPredicate[A]): MyList[A] = {
+    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+    else t.filter(predicate)
+  }
+
+  /**
+    * [1, 2] ++ [3,4,5]
+    * = new Cons(1, [2] ++ [3,4,5])
+    * = new Const(1, new Cons(2, Empty ++ [3,4,5]))
+    * = new Cons(1, new Cons(2, new Cons(3, new Cons(4, new Cons(5, Empty))))
+    **/
+  override def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
+}
+
+trait MyPredicate[-A] {
+  def test(element: A): Boolean
+}
+
+trait MyTransform[-A, B] {
+  def transform(element: A): B
 }
 
 object ListTest extends App {
@@ -62,4 +113,23 @@ object ListTest extends App {
 
   val listOfString = new Cons("Hello", new Cons("Scala", Empty))
   println(listOfString.toString)
+
+  val greaterThanTwo = new MyPredicate[Int] {
+    def test(element: Int): Boolean = element > 2
+  }
+
+  println(list2.filter(greaterThanTwo))
+
+  val doubled = new MyTransform[Int, Int] {
+    def transform(element: Int): Int = element * element
+  }
+  println(list2.map(doubled))
+
+  println(list1 ++ list2)
+
+  val expendElement = new MyTransform[Int, MyList[Int]] {
+    def transform(element: Int): MyList[Int] = new Cons(element, new Cons(element + 1, Empty))
+  }
+
+  println(list2.flatMap(expendElement))
 }
