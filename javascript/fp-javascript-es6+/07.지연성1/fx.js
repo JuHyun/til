3ca0a2f81dff9e7ecc4e7f70c2be2de0456b0558
Estel Seong <estel.castle@gmail.com>
2019-09-16 23:41:10 +0900

    07. 지연성1 - take.

--- javascript/fp-javascript-es6+/07.지연성1/fx.js
+++ javascript/fp-javascript-es6+/07.지연성1/fx.js
@@ -1,6 +1,8 @@
 const log = console.log;
 
-const reduce = (fn, acc, iter) => {
+const curry = (f) => (a, ...args) => args.length ? f(a, ...args) : (...args) => f(a, ...args);
+
+const reduce = curry((fn, acc, iter) => {
   if (!iter) {
     iter = acc[Symbol.iterator]();
     acc = iter.next().value;
@@ -9,4 +11,6 @@ const reduce = (fn, acc, iter) => {
     acc = fn(acc, v);
   }
   return acc;
-};
+});
+
+const go = (...args) => reduce((a, f) => f(a), args);
