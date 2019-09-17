d020cc43ec58fc6b1279df0f94764c4acc9b602a
Estel Seong <estel.castle@gmail.com>
2019-09-17 23:14:05 +0900

    range, map, filter, take, reduce ᄌ첩 사용 vs L.range, L.map, L.filter, take 의 평가 순서

--- javascript/fp-javascript-es6+/07.지연성1/fx.js
+++ javascript/fp-javascript-es6+/07.지연성1/fx.js
@@ -1,16 +1,8 @@
 const log = console.log;
 
-const curry = (f) => (a, ...args) => args.length ? f(a, ...args) : (...args) => f(a, ...args);
-
-const reduce = curry((fn, acc, iter) => {
-  if (!iter) {
-    iter = acc[Symbol.iterator]();
-    acc = iter.next().value;
-  }
-  for (const v of iter) {
-    acc = fn(acc, v);
-  }
-  return acc;
-});
+const curry = f =>
+  (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
 const go = (...args) => reduce((a, f) => f(a), args);
+
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
